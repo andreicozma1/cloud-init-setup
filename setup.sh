b66097616a004c82e655c34b252bd6eb77969c94
Andrei Cozma <me@andreicozma.com>
2021-12-30 15:39:58 -0500

    Quick update/fix

--- setup.sh
+++ setup.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 
 # Install packages with APT
-apt install git git-lfs
+apt install -y git git-lfs
 
-apt install make
+apt install -y make
 
-apt install zip
+apt install -y zip
 
-apt install ncdu
+apt install -y ncdu
 
-apt install vim
+apt install -y vim
 
-apt install ufw fail2ban
+apt install -y ufw fail2ban
 
 # apt install zsh
 
-apt install python3-pip python3-venv
+apt install -y python3-pip python3-venv
 
-apt install libusb-1.0-0-dev wiringpi raspi-config
+apt install -y libusb-1.0-0-dev wiringpi raspi-config
 
-apt install traceroute net-tools
+apt install -y traceroute net-tools
 
-apt install nginx
+apt install -y nginx
 
 # UFW - Firewall Test
 echo "# Testing UFW"
 ufw reload
 ufw status
 
-
 # Install WebMin
 echo "# Installing Webmin"
+sudo add-apt-repository "deb [arch=amd64] http://download.webmin.com/download/repository sarge contrib"
+
 cd /root
 wget https://download.webmin.com/jcameron-key.asc
 cat jcameron-key.asc | gpg --dearmor >/etc/apt/trusted.gpg.d/jcameron-key.gpg
 
-apt-get install apt-transport-https
+apt-get install apt-transport-https software-properties-common
 apt-get update
 apt-get install webmin
